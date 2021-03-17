cdf8b1dfa1f9022c7b1dc3ae20832c85a79b17ae
Toe-Tow <62809444+Toe-Tow@users.noreply.github.com>
2021-03-17 11:55:18 -0400

    Add files via upload

---
+++ components/Name.js
@@ -0,0 +1,12 @@
+import React, {Component} from "React";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Name extends Component {
+    render(){
+        return(
+            <view>
+               <Text>Torin Krishnarao</Text>
+            </view>
+        );
+    }
+}
